b074e6689d078f775a9de89974be8a7586525bb5
VENOM-InstantDeath <mohathepro111@gmail.com>
2023-11-01 12:32:26 -0300

    Updated and finished menu

--- main.c
+++ main.c
@@ -26,17 +26,18 @@ void display(WINDOW* win, struct Callback cb, int p, int start, int stop) {
 		mvwaddstr(win, p, 0, cb.ls[start]);
 		return;
 	}
+	int e=0;
 	if (stop>cb.nmemb) stop=cb.nmemb;
 	for (int i=start; i<stop; i++) {
-		mvwaddstr(win, i-start, 0, cb.ls[i]);
+		mvwaddstr(win, e, 0, cb.ls[i]);
+		e++;
 	}
 }
 
 int menu(WINDOW* win, struct Data* data, struct Callback cb, void (*dcb)(WINDOW*,struct Callback,int,int,int)) {
-	int p = 0;
+	int p = 0; int sp = 0;
 	int y,x; getmaxyx(win,y,x);
-	int top=y;
-	dcb(win, cb, p, top-y, y);
+	dcb(win, cb, p, 0, y);
 	wattron(win, COLOR_PAIR(1));
 	if (cb.nmemb) dcb(win, cb, p, 0, -1);
 	wattroff(win, COLOR_PAIR(1));
@@ -47,25 +48,40 @@ int menu(WINDOW* win, struct Data* data, struct Callback cb, void (*dcb)(WINDOW*
 				return 0;
 			case 10:
 				if (!cb.nmemb) break;
-				return cb.foo[p](win, data, cb.args[p]);
+				return cb.foo[sp](win, data, cb.args[sp]);
 			case KEY_UP:
-				if (!cb.nmemb||!p) break;
-				dcb(win, cb, p, p, -1);
+				if (!cb.nmemb||!sp) break;
+				if (!p) {
+					dcb(win, cb, p, sp, -1);
+					wscrl(win, -1);
+					wattron(win, COLOR_PAIR(1));
+					dcb(win, cb, p, sp-1, -1);
+					wattroff(win, COLOR_PAIR(1));
+					sp--;
+					break;
+				}
+				dcb(win, cb, p, sp, -1);
 				wattron(win, COLOR_PAIR(1));
-				dcb(win, cb, p-1, p-1, -1);
+				dcb(win, cb, p-1, sp-1, -1);
 				wattroff(win, COLOR_PAIR(1));
-				p--;
+				p--;sp--;
 				break;
 			case KEY_DOWN:
-				if (!cb.nmemb||p==cb.nmemb-1) break;
-				if (p==top-1) {
-					// scroll
+				if (!cb.nmemb||sp==cb.nmemb-1) break;
+				if (p==y-1) {
+					dcb(win, cb, p, sp, -1);
+					wscrl(win, 1);
+					wattron(win, COLOR_PAIR(1));
+					dcb(win, cb, p, sp+1, -1);
+					wattroff(win, COLOR_PAIR(1));
+					sp++;
+					break;
 				}
-				dcb(win, cb, p, p, -1);
+				dcb(win, cb, p, sp, -1);
 				wattron(win, COLOR_PAIR(1));
-				dcb(win, cb, p+1, p+1, -1);
+				dcb(win, cb, p+1, sp+1, -1);
 				wattroff(win, COLOR_PAIR(1));
-				p++;
+				p++;sp++;
 				break;
 		}
 	}
@@ -80,12 +96,13 @@ int main() {
 	noecho();
 	set_escdelay(100);
 	keypad(stdscr, 1);
+	scrollok(stdscr, 1);
 	init_pair(1, 0, 7);
 	struct Data data;
-	int (*foo[])(WINDOW*,struct Data*,void*) = {print,print,mex};
-	void *args[] = {"Watefoc", "nobro", NULL};
-	char* ls[] = {"Print watefoc", "Print nobro", "Exit"};
-	struct Callback cb = {foo, args, ls, 3};
+	int (*foo[])(WINDOW*,struct Data*,void*) = {print,print,print,print,print,print,mex};
+	void *args[] = {"Watefoc", "nobro", "xd", "xd", "xd", "xd",NULL};
+	char* ls[] = {"Print watefoc", "Print nobro", "xd", "xd", "xd", "xd", "Exit"};
+	struct Callback cb = {foo, args, ls, 7};
 	for (;;) {
 		if (menu(stdscr, &data, cb, display)) {
 			wmove(stdscr, 0, 0); wclrtobot(stdscr);
